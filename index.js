3434c6390844e74716605ba101ed209cf5de38e0
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-05-20 09:51:58 -0300

    Middlewares & DB com Knex #01

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('Meu Backend!')
-})
-
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
