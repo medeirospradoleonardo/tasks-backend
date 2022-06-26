5bc7654fd8ae70a1ad356b5643a5101382e79ab5
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-06-26 20:08:24 -0300

    DB Online

--- config/db.js
+++ config/db.js
@@ -1,5 +1,17 @@
 const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
+// const options = {
+//     client: 'pg',
+//     connection: {
+//       host : 'rosie.db.elephantsql.com',
+//       user : 'iaffojud',
+//       password : '6kAT8c03C9bz3VS6nJ52qDMZpyLtM1Vr',
+//       database : 'iaffojud'
+//     }
+// };
+
+// const knex = require('knex')(options)
+
 knex.migrate.latest([config])
 module.exports = knex
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,23 @@
 // Update with your config settings.
-
 module.exports = {
   client: 'postgresql',
+  // connection: {
+  //   database: 'tasks',
+  //   host : 'localhost',
+  //   user: 'postgres',
+  //   password: '123456'
+  // },
+  // connection: {
+  //   host: 'rosie.db.elephantsql.com',
+  //   user: 'iaffojud',
+  //   password: '6kAT8c03C9bz3VS6nJ52qDMZpyLtM1Vr',
+  //   database: 'iaffojud'
+  // },
   connection: {
-    database: 'tasks',
-    user: 'postgres',
-    password: '123456'
+    host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
+    user: 'llxzfvqmelnxxg',
+    password: '6cb814519ab185aacbcfa8606e9bb9886568a008b5fc04a57eed770c523bbb16',
+    database: 'dajmqbbidfu1tj',
   },
   pool: {
     min: 2,
