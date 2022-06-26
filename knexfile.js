9ae3ba5b044f6f42c1e60ad39977c966cda88fb9
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-06-26 20:34:59 -0300

    Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,20 @@
-// Update with your config settings.
 module.exports = {
   client: 'postgresql',
+  // Conexão Postgres local
   // connection: {
   //   database: 'tasks',
   //   host : 'localhost',
   //   user: 'postgres',
   //   password: '123456'
   // },
+  // Conexão Postgres Elephant
   // connection: {
   //   host: 'rosie.db.elephantsql.com',
   //   user: 'iaffojud',
   //   password: '6kAT8c03C9bz3VS6nJ52qDMZpyLtM1Vr',
   //   database: 'iaffojud'
   // },
+  // Conexão Postgres Heroku
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
